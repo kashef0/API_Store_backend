bbc3d8fc54497f2dad551cdea7541019ec5bdc93
M.kashef <kashef.sy@gmail.com>
2024-05-23 00:58:13 +0200

    la till register route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 userCheck = require("../monitor/userCheck");
 
+router.get("/register", userCheck.register);
 router.post("/login", userCheck.login);
 router.post("/register", userCheck.register);
 
